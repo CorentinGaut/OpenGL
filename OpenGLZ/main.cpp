f69492ae97c6f964269de046c16122c92f693964
Corentin <corentin.chignin@hotmail.fr>
2019-11-19 15:47:02 +0100

    Frame buffer reussi

--- OpenGLZ/main.cpp
+++ OpenGLZ/main.cpp
@@ -1,6 +1,5 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/vec3.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
@@ -13,12 +12,15 @@
 #include <string>
 
 #define TINYPLY_IMPLEMENTATION
+
 #include <tinyply.h>
 #include "stl.h"
 #include "texture.h"
 #include "OBJLoader.h"
 
 float distancez = 10.0f;
+int width, height;
+
 
 static void error_callback(int /*error*/, const char* description)
 {
@@ -224,14 +226,12 @@ int main(void)
 	GLuint vbo, vao;
 	glGenBuffers(1, &vbo);
 	glGenVertexArrays(1, &vao);
-
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, cubeVert.size() * sizeof(Vertex), cubeVert.data(), GL_STATIC_DRAW);
 
 	//position
 	const auto index = glGetAttribLocation(program, "position");
-
 	glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
 	glEnableVertexAttribArray(index);
 
@@ -241,60 +241,59 @@ int main(void)
 	glVertexAttribPointer(indexNormal, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3) * 2, (void*)(sizeof(glm::vec3) + sizeof(glm::vec2)));
 	glEnableVertexAttribArray(indexNormal);*/
 
-	//Texture
-	GLuint textureId = 1;
+	//---------------------------Texture --- de BOIS---------------------------------------------------------
+	GLuint textureId;
 	glCreateTextures(GL_TEXTURE_2D, 1, &textureId);
 	glTextureStorage2D(textureId, 1, GL_RGB8, texture.width, texture.height);
 	glTextureSubImage2D(textureId,0,0,0,texture.width,texture.height,GL_RGB,GL_UNSIGNED_BYTE,texture.data.data());
-
-	glBindTextureUnit(0, textureId);
 	const auto indextexture = glGetAttribLocation(program, "uv_in");
-
 	glVertexAttribPointer(indextexture, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec3)));
 	glEnableVertexAttribArray(indextexture);
-
-
 	int uniformTexture = glGetUniformLocation(program, "Texture");
 	glProgramUniform1i(program, uniformTexture, 0);
 
+	//----------------------------------------texture frame buffer color--------------------------------------
+	GLuint textureColorFrameBuffer;
+	glCreateTextures(GL_TEXTURE_2D, 1, &textureColorFrameBuffer);
+	glTextureStorage2D(textureColorFrameBuffer, 1, GL_RGB8, 500, 500);
+
+	//----------------------------------------texture frame buffer Depth----------------------------------------
+	GLuint textureDepthFrameBuffer;
+	glCreateTextures(GL_TEXTURE_2D, 1, &textureDepthFrameBuffer);
+	glTextureStorage2D(textureDepthFrameBuffer, 1, GL_DEPTH_COMPONENT16, 500, 500);
+
 	//FRAME BUFFER
-	/*GLuint frameBufferId = 1;
+	GLuint frameBufferId = 1;
 	glCreateFramebuffers(1, &frameBufferId);
+	glNamedFramebufferTexture(frameBufferId, GL_COLOR_ATTACHMENT0, textureColorFrameBuffer, 0);
+	glNamedFramebufferTexture(frameBufferId, GL_DEPTH_ATTACHMENT, textureDepthFrameBuffer, 0);
+
 	//glNamedFramebufferTexture()
 	//glBindframebuffer*/
 
 
-	float scale = 0.01f;
+	float scale = 1.0f;
 	glm::mat4 scaleMat = glm::scale(glm::vec3(scale, scale, scale));
 
 	float rotate = 0.0f;
 	float angle = 90.0f;
-	float rad = angle * 180.0 / 3.14;
+	float rad = angle * 180.0 / 3.1415;
 
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
 	while (!glfwWindowShouldClose(window))
 	{
-
 		glfwSetScrollCallback(window, scroll_callback);
-
-		int width, height;
-
 		glfwGetFramebufferSize(window, &width, &height);
 
-		glViewport(0, 0, width, height);
-	
-		glClear(GL_COLOR_BUFFER_BIT);
-		//HOUSE TARGARYAN
-		
+		//CUBE
 			//couleur
-			/*glm::vec3 green = glm::vec3(0.f, 1.f, 1.f);
+			glm::vec3 green = glm::vec3(0.f, 1.f, 1.f);
 			int uniformCouleur = glGetUniformLocation(program, "color");
 			glProgramUniform3f(program, uniformCouleur, green.x, green.y, green.z);
 		
 			//-----------------------------TRANSFORM-----------------------------
-			rotate += 0.05f;
-
+			angle += 0.05f;
 			glm::mat4 rotateMat = glm::rotate(angle, glm::vec3(0.0f, 1.0f, 1.0f));
 			glm::mat4 mat = scaleMat * rotateMat;
 			int uniformTransform = glGetUniformLocation(program, "Mat");
@@ -310,35 +309,22 @@ int main(void)
 			int uniformView = glGetUniformLocation(program, "view");
 			glProgramUniformMatrix4fv(program, uniformView, 1, GL_FALSE, &view[0][0]);
 
-			//glClear(GL_DEPTH_BUFFER_BIT);*/
 
+			//je dessine sur le frame buffer
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, frameBufferId);
+			glViewport(0, 0, 500, 500);
+			glBindTextureUnit(0, textureId);
+			glClearColor(0.6f, 0.6f, 0.6f, 1);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			glDrawArrays(GL_TRIANGLES, 0, verticesOBJ.size() * 3);
 
-		
-		//DEUXIEME OBJET
-		/*
-			//couleur
-			glm::vec3 blue = glm::vec3(0.f, 0.f, 1.f);
-			//uniformCouleur = glGetUniformLocation(program, "color");
-			glProgramUniform3f(program, uniformCouleur, blue.x, blue.y, blue.z);
-
-			//-----------------------------TRANSFORM-----------------------------
-			scaleMat = glm::scale(glm::vec3(0.02f, 0.02f, 0.02f));
-			mat = scaleMat;
-			//uniformTransform = glGetUniformLocation(program, "Mat");
-			glProgramUniformMatrix4fv(program, uniformTransform, 1, GL_FALSE, &mat[0][0]);
-
-			//-----------------------------PROJECTION-----------------------------
-			//uniformProjection = glGetUniformLocation(program, "projection");
-			glProgramUniformMatrix4fv(program, uniformProjection, 1, GL_FALSE, &projection[0][0]);
-
-			//-----------------------------VIEW-----------------------------
-			//uniformView = glGetUniformLocation(program, "view");
-			glProgramUniformMatrix4fv(program, uniformView, 1, GL_FALSE, &view[0][0]);
-			*/
-
-			//glDrawArrays(GL_TRIANGLES, 0, verticesOBJ.size());
-			
+			//je dessine sur le frame buffer de la fenetre
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+			glViewport(0, 0, width, height);
+			glBindTextureUnit(0, textureColorFrameBuffer);
+			glClearColor(0.2f, 0.2f, 0.2f, 1);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glDrawArrays(GL_TRIANGLES, 0, verticesOBJ.size() * 3);
 			
 		glfwSwapBuffers(window);
 		glfwPollEvents();
